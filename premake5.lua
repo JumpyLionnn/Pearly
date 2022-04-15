4c7e73fd734d1ad4665ae2ded18518f8abf4e3b9
JumpyLionnn <fwipsity@gmail.com>
2022-04-15 12:53:53 +0300

    added a build system

---
+++ premake5.lua
@@ -0,0 +1,92 @@
+workspace "Pearly"
+	architecture "x64"
+	configurations 
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Pearly"
+	location "Pearly"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines "PR_PLATFORM_WINDOWS"
+
+	filter "configurations:Debug"
+		defines "PR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PR_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Pearly/vendor/spdlog/include",
+		"Pearly/src"
+	}
+
+	links
+	{
+		"Pearly"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines "PR_PLATFORM_WINDOWS"
+
+	filter "configurations:Debug"
+		defines "PR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PR_DIST"
+		optimize "On"
